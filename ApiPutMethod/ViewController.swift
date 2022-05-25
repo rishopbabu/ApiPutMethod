33f4c60efbb7e1845bc91d85820397580896620a
Rishop Babu <rishop.babu@optisolbusiness.com>
2022-05-25 11:09:15 +0530

    Initial Commit

---
+++ ApiPutMethod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiPutMethod
+//
+//  Created by MAC-OBS-26 on 25/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
