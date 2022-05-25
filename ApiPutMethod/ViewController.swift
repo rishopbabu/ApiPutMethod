964411d2f28020b03563f31af9896ba6751fc928
Rishop Babu <rishop.babu@optisolbusiness.com>
2022-05-25 11:15:49 +0530

    first commit

--- ApiPutMethod/ViewController.swift
+++ ApiPutMethod/ViewController.swift
@@ -7,13 +7,104 @@
 
 import UIKit
 
+typealias Parameters =  [String: String]
+
+struct Media {
+    let key: String
+    let filename: String
+    let data: Data
+    let mimeType: String
+    
+    init?(withImage image: UIImage, forKey key: String) {
+        self.key = key
+        self.mimeType = "image/jpeg"
+        self.filename = "photo\(arc4random()).jpeg"
+        
+        guard let data = image.jpegData(compressionQuality: 0.5) else { return nil }
+        //guard let data = image.pngData() else { return nil }
+        //guard let data = image.imageAsset else { return nil }
+        
+        self.data = data
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        uploadImage()
     }
-
-
+    
+    func uploadImage() {
+        
+        let parameters = ["userId": "101",
+                          "useCase": "wechat"]
+        
+        guard let mediaImage = Media(withImage: UIImage(named: "photo")!, forKey: "file") else { return }
+        
+        guard let url = URL(string: "http://54.235.130.26:8501/optiscan/uploadfile") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        
+        let boundary = generateBoundary()
+        
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        let dataBody = createDataBody(withParameters: parameters, media: [mediaImage], boundary: boundary)
+        request.httpBody = dataBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
+    
+    func generateBoundary() -> String {
+        return "Boundary-\(NSUUID().uuidString)"
+    }
+    
+    func createDataBody(withParameters params: Parameters?, media: [Media]?, boundary: String) -> Data {
+        
+        let lineBreak = "\r\n"
+        var body = Data()
+        
+        if let parameters = params {
+            for (key, value) in parameters {
+                body.append("--\(boundary + lineBreak)")
+                body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)")
+                body.append("\(value + lineBreak)")
+            }
+        }
+        if let media = media {
+            for photo in media {
+                body.append("--\(boundary + lineBreak)")
+                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename= \"\(photo.filename)\"\(lineBreak)")
+                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)")
+                body.append(photo.data)
+                body.append(lineBreak)
+            }
+        }
+        
+        body.append("--\(boundary)--\(lineBreak)")
+        return body
+    }
 }
 
+extension Data {
+    mutating func append(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            append(data)
+        }
+    }
+}
