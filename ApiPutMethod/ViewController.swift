7be6411da7aca8e5520656b3c7c595ed2f3bd6a6
Rishop Babu <rishop.babu@optisolbusiness.com>
2023-02-03 00:35:21 -0700

    video upload try.

--- ApiPutMethod/ViewController.swift
+++ ApiPutMethod/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by MAC-OBS-26 on 25/05/22.
 //
+// "/Users/mac-obs-26/Downloads/video.mp4"
 
 import UIKit
 
@@ -12,22 +13,37 @@ typealias Parameters =  [String: String]
 struct Media {
     let key: String
     let filename: String
-    let data: Data
+    //let data: Data
     let mimeType: String
     
     init?(withImage image: UIImage, forKey key: String) {
         self.key = key
         self.mimeType = "image/jpeg"
         self.filename = "photo\(arc4random()).jpeg"
-        
-        guard let data = image.jpegData(compressionQuality: 0.5) else { return nil }
-        
+//        guard let data = Data(String: String.Encoding.utf8) else { return nil }
+//        self.data = data
+    }
+}
+
+struct MediaTwo {
+    let key: String
+    let filename: String
+    let data: Data
+    let mimeType: String
+
+    init?(withData url: URL?, forKey key: String) {
+        self.key = key
+        self.mimeType = "video/mp4"
+        self.filename = "video.mp4"
+
+        guard let data = try? Data(contentsOf: url ?? URL(fileURLWithPath: "")) else { return nil }
+
         self.data = data
     }
 }
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         uploadImage()
@@ -35,21 +51,39 @@ class ViewController: UIViewController {
     
     func uploadImage() {
         
-        let parameters = ["userId": "101",
-                          "useCase": "wechat"]
+        let parameters = ["title": "sampletest",
+                          "channelId": "61c2f3772e0cbd194220a30b",
+                          "categoryId": "61cda3a28d78564104af13e8",
+                          "subCategoryId": "61dc18a49264c843dc187dc3",
+                          "tags": "Testtwotesttwo",
+                          "isUploadedByFan": "false" ]
         
-        guard let mediaImage = Media(withImage: UIImage(named: "photo")!, forKey: "file") else { return }
+        //let image = UIImage(named: "photo")
+        //guard let mediaImage = Media(withImage: image ?? UIImage(), forKey: "file") else { return }
+       
+        //let fileURL = URL(string: "/Users/mac-obs-26/Downloads/video.mp4")!
+        //let fileData = try? Data(contentsOf: fileURL)
+        //guard let fileData = MediaTwo(withData: fileData, forKey: "file") else { return }
         
-        guard let url = URL(string: "http://54.235.130.26:8501/optiscan/uploadfile") else { return }
+        //let userID = "639aa2616cd74d004c0f929c"
+        guard let url = URL(string: "https://api.fanatictv.com/api/v2/video/create_fan_video") else { return }
         var request = URLRequest(url: url)
-        request.httpMethod = "PUT"
+        request.httpMethod = "POST"
         
         let boundary = generateBoundary()
+        let tokenStr = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmYW5hdGljVHYiLCJzdWIiOnsidXNlcklkIjoiNjM5YWEyNjE2Y2Q3NGQwMDRjMGY5MjljIn0sImlhdCI6MTY3MTcxMTA3MTE5NH0.CVExN2khE53t1Q64pt9YLhE5_pHtcZVA_Dxba6PuQe4"
         
         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.setValue("jwt \(tokenStr)", forHTTPHeaderField: "Authorization")
         
-        let dataBody = createDataBody(withParameters: parameters, media: [mediaImage], boundary: boundary)
+        let videoURL = URL(fileURLWithPath: "/Users/mac-obs-26/Downloads/video.mp4")
+        let videoURLData = MediaTwo(withData: data, forKey: "file")
+        
+        let dataBody = createDataBody(withParameters: parameters, media: videoURLData, boundary: boundary)
         request.httpBody = dataBody
+        
+        print(request.httpBody)
+        print(dataBody)
         
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
@@ -59,6 +93,7 @@ class ViewController: UIViewController {
             
             if let data = data {
                 do {
+                    print(String(data: data, encoding: .utf8) as Any)
                     let json = try JSONSerialization.jsonObject(with: data, options: [])
                     print(json)
                 } catch {
@@ -72,8 +107,34 @@ class ViewController: UIViewController {
         return "Boundary-\(NSUUID().uuidString)"
     }
     
-    func createDataBody(withParameters params: Parameters?, media: [Media]?, boundary: String) -> Data {
-        
+//    func createDataBody(withParameters params: Parameters?, media: Media?, boundary: String) -> Data {
+//
+//        let lineBreak = "\r\n"
+//        var body = Data()
+//
+//        if let parameters = params {
+//            for (key, value) in parameters {
+//                body.append("--\(boundary + lineBreak)")
+//                body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)")
+//                body.append("\(value + lineBreak)")
+//            }
+//        }
+//        if let media = media {
+//            for photo in media {
+//                body.append("--\(boundary + lineBreak)")
+//                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename= \"\(photo.filename)\"\(lineBreak)")
+//                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)")
+//                body.append(photo.data)
+//                body.append(lineBreak)
+//            }
+//        }
+//
+//        body.append("--\(boundary)--\(lineBreak)")
+//        return body
+//    }
+    
+    func createDataBody(withParameters params: Parameters?, media: MediaTwo?, boundary: String) -> Data {
+
         let lineBreak = "\r\n"
         var body = Data()
         
@@ -85,15 +146,13 @@ class ViewController: UIViewController {
             }
         }
         if let media = media {
-            for photo in media {
-                body.append("--\(boundary + lineBreak)")
-                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename= \"\(photo.filename)\"\(lineBreak)")
-                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)")
-                body.append(photo.data)
-                body.append(lineBreak)
-            }
+            body.append("--\(boundary)\r\n")
+            body.append("Content-Disposition: form-data; name=\"\(media.key)\"; filename= \"\(media.filename)\"\(lineBreak)")
+            body.append("Content-Type: \(media.mimeType + lineBreak + lineBreak)")
+            body.append(media.data)
+            body.append(lineBreak)
         }
-        
+
         body.append("--\(boundary)--\(lineBreak)")
         return body
     }
